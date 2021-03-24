af630d0b209d1e17e993f6e43d546995ca7ff11e
aaaryya1 <79969052+aaaryya1@users.noreply.github.com>
2021-03-24 21:37:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import graphics.circle as ci
-import graphics.rectangle as re
-import graphics.dgraphics.cuboid as cu
-import graphics.dgraphics.sphere as sp
-print("area of circle",ci.area(3))
-print("perimeter of circle",ci.perimeter(3))
-print("area of rectangle",re.area(2,8))
-print("perimeter of rectangle",re.perimeter(2,8))
-print("area of cuboid",cu.area(2,8,5))
-print("perimeter of cuboid",cu.perimeter(2,8,5))
-print("area of sphere ",sp.area(2))
-print("peremeter of sphere",sp.perimeter(2))
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
